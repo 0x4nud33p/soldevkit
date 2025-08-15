5a133add0d2d4e3d52999d712c188a35b7123f12
satyawaniaman <amansatyawani10@gmail.com>
2025-08-16 01:48:41 +0530

    refactor(hero): remove unused ManifestoSection export

--- src/components/hero/index.ts
+++ src/components/hero/index.ts
@@ -1,6 +1,5 @@
 export { Header } from "./header";
 export { HeroSection } from "./hero-section";
-export { ManifestoSection } from "./manifesto-section";
 export { FeaturesSection } from "./features-section";
 export { Footer } from "./footer";
 export { TeamMember } from "./team-member";
